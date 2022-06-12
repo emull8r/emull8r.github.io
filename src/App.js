56f2adcbfdbf7f5d4a2b23ee5ebd101707cccd2f
emull8r <86169657+emull8r@users.noreply.github.com>
2022-06-12 18:48:35 -0400

    Removed navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from './pages';
-import About from './pages/about';
-import Contact from './pages/contact';
 
 function App() {
   return (
     
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-      </Routes>
-    </HashRouter>
+    <h1>Test</h1>
     
   );
 }
