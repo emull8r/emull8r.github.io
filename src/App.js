6c27954c133228be823d32b4f0c91fab85c79015
emull8r <86169657+emull8r@users.noreply.github.com>
2022-06-12 18:40:17 -0400

    Switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages';
 import About from './pages/about';
 import Contact from './pages/contact';
@@ -9,14 +9,14 @@ import Contact from './pages/contact';
 function App() {
   return (
     
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path='/' exact component={Home} />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 }
