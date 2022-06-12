f3f250a57b94740bdd68d955add4a37ec0bd474f
emull8r <86169657+emull8r@users.noreply.github.com>
2022-06-12 19:24:25 -0400

    Attempted to fix router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar';
+import { Router, Routes, Route } from "react-router-dom";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
 
 function App() {
   return (
     
-    <h1>Test</h1>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/emull8r.github.io' exact component={Home} />
+        <Route path='/emull8r.github.io/about' component={About} />
+        <Route path='/emull8r.github.io/contact' component={Contact} />
+      </Routes>
+    </Router>
     
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-
+import {useHistory} from 'react-router-dom'
 
 const Home = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Home</h1>
-	</div>
-);
-};
+    let history = useHistory()
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={()=>history.push('/emull8r.github.io/about')}>Click here</button>
+        </div>
+    )
+}
 
 export default Home;
