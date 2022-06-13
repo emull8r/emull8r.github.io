00e900cc6908d277eb83fc65ac307ac30eafb5a2
emull8r <86169657+emull8r@users.noreply.github.com>
2022-06-12 21:43:09 -0400

    Adjusted homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom'
 
 const Home = () => {
-    let history = useHistory()
     return (
         <div>
             <h1>Home</h1>
-            <button onClick={()=>history.push('/emull8r.github.io/about')}>Click here</button>
         </div>
     )
 }
