8dff647b1d6286ce465e135617f980a796d0fc45
Evan Mulloy <evmulloy@gmail.com>
2022-06-13 08:42:44 -0400

    Adjusted router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
-import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages';
 import About from './pages/about';
 import Contact from './pages/contact';
 
 function App() {
   return (
-    
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-      </Routes>
-    </HashRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </Routes>
+      </div>
+      
     
   );
 }
