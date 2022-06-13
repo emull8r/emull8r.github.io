578c3ba06e997b72759b7a2f84550e50377bf1d5
emull8r <86169657+emull8r@users.noreply.github.com>
2022-06-12 22:08:56 -0400

    Made app return home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import { Router, Routes, Route } from "react-router-dom";
+//import Navbar from './components/navbar';
+//import { Router, Routes, Route } from "react-router-dom";
 import Home from './pages';
 import About from './pages/about';
 import Contact from './pages/contact';
 
 function App() {
   return (
-    
+    <Home />
+    /*
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/emull8r.github.io' exact component={Home} />
-        <Route path='/emull8r.github.io/about' component={About} />
-        <Route path='/emull8r.github.io/contact' component={Contact} />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
       </Routes>
     </Router>
-    
+    */
   );
 }
 
